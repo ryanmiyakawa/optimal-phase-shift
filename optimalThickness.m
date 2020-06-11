46c869e34cc27287807c53245ddca565070c905b
ahxie <ahxie@iu.edu>
2020-06-11 15:38:34 -0400

    fixed the n2 error, cant get the units to change

--- optimalThickness.m
+++ optimalThickness.m
@@ -16,7 +16,6 @@
 
 my-variable
 
-
 2. camel case
 
 myGreatVariable
@@ -25,42 +24,41 @@ myGreatVariable
 
 my_variable
 
-
-
 %}
 
 
 function d = optimalThickness(n2, phaseShift)
 
+%{
+function: calculates optimal thickness as a function of phase shift
+inputs: n2, theta1, phaseShift
+output: d
+%}
+
 lambda = 13.5;
 n1 = 1;
 theta1 = 6*pi/180; % 6 degrees
-theta2 = asin(n1/n2 * sin(theta1));
+theta2 = asin(n1/n2*sin(theta1));
 
-
-
-
-d = phaseShift * lambda/(4*pi * (n1/cos(theta1) - n2/cos(theta2) ));
-
+d = phaseShift*lambda/(4*pi*(n1/cos(theta1)-n2/cos(theta2)));
 
 % if d is negative, then this is not realistic, and we need to look for
 % another solution
 
-
 counter = 0;
 while (d < 0)
-    % Then we know that d is too small, and we'd like to choose the next
+    % then we know that d is too small, and we'd like to choose the next
     % value
     
-    phaseShift = phaseShift - 2*pi;
+    phaseShift = phaseShift-2*pi;
     counter = counter + 1;
     
-    d = (phaseShift) * lambda/(4*pi * (n1/cos(theta1) - n2/cos(theta2) ));
+    d = (phaseShift)*lambda/(4*pi*(n1/cos(theta1)-n2/cos(theta2)));
     
     
-    if (counter > 50)
-        disp('Were likely in an infinite loop')
-        break %abort the while loop
+    if (counter>50)
+        disp('likely in an infinite loop')
+        break % abort the while loop
     end
     
 end
