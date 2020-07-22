32054df7a2c252937907489b1af463e6430218c8
ahxie <ahxie@iu.edu>
2020-07-22 17:48:25 -0400

    added blur
    to take into account other stochasitc effects

--- scratch.m
+++ scratch.m
@@ -1,49 +1,23 @@
 
-grating = zeros(1, N);
-numOfPeriods = 5;
+%{
+convolve wafer image with gaussian
+1. define gaussian image as fxn of fwhm
+    - define fwhm
+2. determine what to convolve it with
+    - the bit that has the shot noise
+3. do the convolution
+4. plot the blurred image
+%}
 
-for i = 1:(numOfPeriods*4)
-    if mod(i, 4) == 1
-        region1 = (x >= boundaries(i) & x <= boundaries(i+1));
-        E1 = E0*(exp(1i*(2*pi*L)/lambda));
-        grating(region1) = E1;
-    elseif mod(i, 4) == 2
-        region2 = (x >= boundaries(i) & x <= boundaries(i+1));
-        x2 = abs(x(region2)-((i/4)*pitch-(2*d*tand(6))));
-        path2a = abs(((L.*sind(6))-x2./(sind(6)))); 
-        path2b = (L - path2a);
-        E2 = E0.*exp(1i.*(((2.*pi.*path2a)./lambda)+((2.*pi.*path2b.*n2)./lambda))).*exp(-((2.*pi.*beta).*path2b)./lambda);
-        grating(region2) = E2;
-    elseif mod(i, 4) == 3
-        region3 = (x >= boundaries(i) & x <= boundaries(i+1));
-        E3 = E0*exp(1i*((2*pi*L*n2)/lambda))*exp(-(2*pi*L*beta)/lambda);
-        grating(region3) = E3;
-    elseif mod(i, 4) == 0
-        region4 = (x >= boundaries(i) & x <= boundaries(i+1));
-        path4a = abs((x(region4)-pitch)./sind(6)); 
-        path4b = L - path4a;
-        E4 = E0.*exp(1i.*(((2.*pi.*path4a.*n2)./lambda)+((2.*pi.*path4b)./lambda))).*exp(-(2.*pi.*beta.*path4a)./lambda);
-        grating(region4) = E4;
-    else 
-        disp('whoops something went wrong');
-    end
-end
+% defining gaussian as fxn of fwhm
+range = 100;
+x = linspace(1, range, 100);
+fwhm = 20;
+sigma = fwhm/(2*sqrt(2*log(2)));
+mu = range/2;
+gaussian = (1/(sigma*sqrt(2*pi))).*exp(-((x-mu).^2)./(2.*sigma^2));
+plot(x, gaussian)
 
-a = linspace(1, 100, 100);
-b = [0];
-for i = 1:10
-    b = [b, 10*i];
-end
-
-smth = zeros(1, 100);
-for i = 1:10
-    if mod(i, 2) == 1
-        cOdd = (a >= b(i) & a <= b(i+1));
-        smth(cOdd) = (i*2);
-    elseif mod(i, 2) == 0
-        cEven = a >= b(i) & a <= b(i+1);
-        smth(cEven) = (i*2);
-    else
-        disp('somethings not right')
-    end
-end
+% convolve with some vector
+vector = linspace(1, 100, 100);
+convolution = conv(vector, gaussian);
