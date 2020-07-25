4be292874f4fbcacc19e66912452381b223818c3
ahxie <ahxie@iu.edu>
2020-07-25 15:35:15 -0400

    figured out N issue

--- scratch.m
+++ scratch.m
@@ -6,15 +6,16 @@ why does changing my N make my fft narrower?
 2. plot grating in N = 1000 and N = 2000
 %}
 
-% N = 500;
-% pitch = 200;
-% periods = 5;
-% x = linspace(0, pitch*periods, N);
-% grating = square((x.*(2*pi))./(pitch))+1;
+N = 1000; % in units of pixels
+pitch = 200;
+periods = 5;
+x = linspace(0, pitch*periods, N+1); % in units of space
+x(end) = [];
+grating = square((x.*(2*pi))./(pitch))+1;
 
-N = 2000;
-x = linspace(0, 10*pi, N);
-grating = square(x);
+% N = 2000;
+% x = linspace(0, 10*pi, N);
+% grating = square(x);
 
 % plot the grating
 figure(1)
@@ -22,4 +23,4 @@ plot(x, grating)
 
 % plot the fft
 figure(2)
-plot(x, abs(fftshift(fft(grating))))
+plot(abs(fftshift(fft(grating))))
